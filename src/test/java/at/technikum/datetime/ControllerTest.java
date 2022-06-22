a30b8036c26eca79df1a54fd422dd05310611d64
Kamil Straczek <wi21b019@technikum-wien.at>
2022-06-22 19:37:25 +0200

    added the routes and methods

---
+++ src/test/java/at/technikum/datetime/ControllerTest.java
@@ -0,0 +1,28 @@
+package at.technikum.datetime;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ControllerTest {
+
+    DateTimeController calculator = new DateTimeController();
+    @Test
+    void timetest(){
+        //Arrange
+
+        //Assert
+
+    }
+
+
+
+
+
+
+
+
+}
+
